1aa986b865115f07532a6ffd08a198e4d5d6ef7a
Fabian Schuiki <fabian@schuiki.ch>
2018-11-02 09:25:26 +0100

    fix drawing of sequential impl result
    Fix the `draw-sequential.sh` script.

--- scripts/draw-sequential.sh
+++ scripts/draw-sequential.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 ROOT=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
-"$ROOT"/build/mst-sequential 20 30 > sequential.gv
-dot -Tpdf sequential.gv -o sequential.pdf
+"$ROOT"/build/mst-sequential 20 30
+neato -Tpdf sequential_mst.gv -o sequential.pdf
