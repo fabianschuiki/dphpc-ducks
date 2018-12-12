14a9a4ccf6ca136c3b8c67eb3b5ebe118f48b35d
Fabian Schuiki <fschuiki@iis.ee.ethz.ch>
2018-12-12 14:35:02 +0100

    update draw-boost script
    Turns out that the boost implementation of Prim's algorithm does not
    find forests of MSTs, but simply the MST that goes through the very
    first vertex in the set.
    
    Adjust the `draw-boost.sh` script to produce a sample graph with three
    separate subgraphs, only the first of which actually has its MST
    created.

--- scripts/draw-boost.sh
+++ scripts/draw-boost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 ROOT=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
-"$ROOT"/build/mst-boost "$ROOT"/data/er_20_30.bin
+"$ROOT"/build/mst-boost 8 0.2
 dot -Tpdf boost_mst.gv -o boost.pdf
